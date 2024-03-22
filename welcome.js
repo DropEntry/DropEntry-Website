ddec21c3943439209202e9161a9e69790a036b1c
DropEntry <admin@dropentry.com>
2024-03-23 09:23:17 +1000

    aws testing

--- welcome.js
+++ welcome.js
@@ -1,5 +1,5 @@
 const clientId = '454235B3B68F4B158C9ED03700E0D7B6';
-const redirectUri = encodeURIComponent('https://api.dropentry.com/dev/auth'); // https://7wgnegmbs3.execute-api.ap-southeast-2.amazonaws.com/dev/auth // http://localhost:5500/auth.html
+const redirectUri = encodeURIComponent('https://7wgnegmbs3.execute-api.ap-southeast-2.amazonaws.com/dev/auth'); // https://api.dropentry.com/dev/auth // https://7wgnegmbs3.execute-api.ap-southeast-2.amazonaws.com/dev/auth // http://localhost:5500/auth.html
 const authUrl = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=openid profile email files accounting.transactions accounting.attachments accounting.contacts accounting.settings offline_access`;
 
 
@@ -14,5 +14,4 @@ const authenticate = () => {
 
 const authButton = document.getElementById("authButton");
 authButton.addEventListener("click", authenticate);
-
 console.log("welcome.js"); 
